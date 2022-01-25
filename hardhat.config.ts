b4e7bfc0b56bc122e6d4a056943c94c3c1176e7b
AliyaDav <aliya_dav98@mail.ru>
2022-01-25 11:44:37 +0100

    deployed new contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-import "./tasks/ERC20Tasks";
+import "./tasks/ERC20-Tasks";
 
 dotenv.config();
 
