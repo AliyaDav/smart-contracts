412e3c9bdd4277cf074c73f7eab0afda7a3f18cb
AliyaDav <aliya_dav98@mail.ru>
2022-01-26 18:30:38 +0100

    added etherscan api token

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,7 @@ dotenv.config();
 
 const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
 const ALCHEMY_PROJECT_ID = process.env.ALCHEMY_PROJECT_ID;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
