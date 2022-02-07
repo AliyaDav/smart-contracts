265c7eb909c54703cacda83d94cff72f354daa5f
AliyaDav <aliya_dav98@mail.ru>
2022-02-07 18:40:28 +0100

    renamed

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract MyContract is ERC1155Holder {}
+contract FundingPlatform is ERC1155 {
+    /*
+     Users of the platform are those who want to raise money for their project.
+    */
+}
